6c4f3449fe7d9cd5672cd87a3d44269b6061f2d2
Ting <tzhang85@gatech.edu>
2014-05-21 15:08:02 -0400

    db stuff, welcome file created

---
+++ treat-yo-self/src/com/example/treat_yo_self/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.treat_yo_self;
+
+import com.example.treat_yo_self.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * The Class Welcome.
+ * 
+ * This is the first activity, or screen, for our android application.
+ * 
+ * @see SystemUiHider
+ */
+public class WelcomeActivity extends Activity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_welcome);
+
+	}
+
+	
+}
