60a29d22e2bf10fc4053be3dc11b11c7f1b6eaa4
Ting <tzhang85@gatech.edu>
2014-05-21 18:27:00 -0400

    Welcome screen

--- treat-yo-self/src/com/example/treat_yo_self/Account.java
+++ treat-yo-self/src/com/example/treat_yo_self/Account.java
@@ -8,36 +8,36 @@ import java.sql.Connection;
 	import java.sql.ResultSet;
 	import java.sql.Statement;
 	
-	import net.sourceforge.jtds.jdbc.*;
+//	import net.sourceforge.jtds.jdbc.*;
 	
-	public void query2()
-	{
-	Log.i("Android"," MySQL Connect Example.");
-	Connection conn = null;
-	try {
-	String driver = "net.sourceforge.jtds.jdbc.Driver";
-	Class.forName(driver).newInstance();
-	//test = com.microsoft.sqlserver.jdbc.SQLServerDriver.class;
-	String connString = "updb1.up.ist.psu.edu;encrypt=false;user=sqlpzw5095;password=RqUGthh6;instance=SQLEXPRESS;";
-	String username = "sqlpzw5095";
-	String password = "RqUGthh6";
-	conn = DriverManager.getConnection(connString,username,password);
-	Log.w("Connection","open");
-	Statement stmt = conn.createStatement();
-	ResultSet reset = stmt.executeQuery("select * from TableName");
-	
-	//Print the data to the console
-	while(reset.next()){
-	Log.w("Data:",reset.getString(3));
-	//              Log.w("Data",reset.getString(2));
-	}
-	conn.close();
-	
-	} catch (Exception e)
-	{
-	Log.w("Error connection","" + e.getMessage());
-	}
-	}
+//	public void query2()
+//	{
+//	Log.i("Android"," MySQL Connect Example.");
+//	Connection conn = null;
+//	try {
+//	String driver = "net.sourceforge.jtds.jdbc.Driver";
+//	Class.forName(driver).newInstance();
+//	//test = com.microsoft.sqlserver.jdbc.SQLServerDriver.class;
+//	String connString = "updb1.up.ist.psu.edu;encrypt=false;user=sqlpzw5095;password=RqUGthh6;instance=SQLEXPRESS;";
+//	String username = "sqlpzw5095";
+//	String password = "RqUGthh6";
+//	conn = DriverManager.getConnection(connString,username,password);
+//	Log.w("Connection","open");
+//	Statement stmt = conn.createStatement();
+//	ResultSet reset = stmt.executeQuery("select * from TableName");
+//	
+//	//Print the data to the console
+//	while(reset.next()){
+//	Log.w("Data:",reset.getString(3));
+//	//              Log.w("Data",reset.getString(2));
+//	}
+//	conn.close();
+//	
+//	} catch (Exception e)
+//	{
+//	Log.w("Error connection","" + e.getMessage());
+//	}
+//	}
 
 
 
