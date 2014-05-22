8b05aba486a34be5d7d8d8c3af724e5020c51df3
Develra <michael.aaron@colorado.edu>
2014-05-22 00:15:22 -0400

    Parse SDK added

---
+++ treat-yo-self/src/com/example/treat_yo_self/DepositActivity.java
@@ -0,0 +1,13 @@
+package com.example.treat_yo_self;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 5/21/2014.
+ */
+public class DepositActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
