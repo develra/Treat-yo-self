2da3657156531b07ac4d7f57c8eb01f58d3fe01f
Develra <michael.aaron@colorado.edu>
2014-05-21 21:58:28 -0400

    Working login screen, no logo

--- treat-yo-self/src/com/example/treat_yo_self/LoginActivity.java
+++ treat-yo-self/src/com/example/treat_yo_self/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.treat_yo_self;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Michael on 5/21/2014.
@@ -11,4 +13,10 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void gtHome(View view) {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
 }
