ad0e2f10a0fb4a87fa68dd009255dafa40a64fb0
Develra <michael.aaron@colorado.edu>
2014-05-22 09:29:49 -0400

    parse test

--- treat-yo-self/src/com/example/treat_yo_self/LoginActivity.java
+++ treat-yo-self/src/com/example/treat_yo_self/LoginActivity.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by Michael on 5/21/2014.
  */
 public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
+        Parse.initialize(this, "IpTVhN6pW1NELloHetmRvoHLzSlnWqVnnTGMBvX5", "KqkQyLx5eXtv3cDib3Phec9F5amTgKGznMn9YWPv");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     public void gtHome(View view) {
