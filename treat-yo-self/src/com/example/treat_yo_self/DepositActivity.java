e33e146065a5a437a6d6f2dfa2289023cf1e1b65
Yossie Rosenstein <yossef.rosenstein@gmail.com>
2014-05-22 10:15:12 -0400

    Star coin pic, Parse file, a midway deposit page

--- treat-yo-self/src/com/example/treat_yo_self/DepositActivity.java
+++ treat-yo-self/src/com/example/treat_yo_self/DepositActivity.java
@@ -9,6 +9,6 @@ import android.os.Bundle;
 public class DepositActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deposity);
+        setContentView(R.layout.activity_deposit);
     }
 }
