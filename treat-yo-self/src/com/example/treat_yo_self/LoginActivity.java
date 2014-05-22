347758c956ab11ec26f5773375dff4b4f2a0a2c5
Develra <michael.aaron@colorado.edu>
2014-05-21 20:11:52 -0400

    login activity start

---
+++ treat-yo-self/src/com/example/treat_yo_self/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.treat_yo_self;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 5/21/2014.
+ */
+public class LoginActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
